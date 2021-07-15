fa78963d14acc1b7cafd8d011fffd1fa72f5f126
Raul-Duarte <raulduarte.info@gmail.com>
2021-07-14 23:13:24 -0300

    feat: buscando dados da api

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,95 +1,39 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import {MdAddShoppingCart} from "react-icons/md"
+import api from '../../server/api'
 
 import {ProductList} from "./styles";
 
 export default function Home() {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        async function loadPorduct() {
+            const response = await api.get('products')
+            setProducts(response.data)
+        }
+
+        loadPorduct()
+    }, [])
+    console.log(products)
     return (
         <ProductList>
-            <li>
-                <img
-                    src="https://static.netshoes.com.br/produtos/tenis-olympikus-attract-se-815-masculino/26/D22-3836-026/D22-3836-026_zoom1.jpg?ts=1585920125&ims=326x"
-                    alt="tenis"
-                />
-                <strong>Tênis muito legal</strong>
-                <span>R$129,00</span>
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart size={16} color="#FFF"/>
-                    </div>
-                    <span>ADICIONAR AO CARRINHHO</span>
-                </button>
-            </li>
-            <li>
-                <img
-                    src="https://static.netshoes.com.br/produtos/tenis-olympikus-attract-se-815-masculino/26/D22-3836-026/D22-3836-026_zoom1.jpg?ts=1585920125&ims=326x"
-                    alt="tenis"
-                />
-                <strong>Tênis muito legal</strong>
-                <span>R$129,00</span>
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart size={16} color="#FFF"/>
-                    </div>
-                    <span>ADICIONAR AO CARRINHHO</span>
-                </button>
-            </li>
-            <li>
-                <img
-                    src="https://static.netshoes.com.br/produtos/tenis-olympikus-attract-se-815-masculino/26/D22-3836-026/D22-3836-026_zoom1.jpg?ts=1585920125&ims=326x"
-                    alt="tenis"
-                />
-                <strong>Tênis muito legal</strong>
-                <span>R$129,00</span>
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart size={16} color="#FFF"/>
-                    </div>
-                    <span>ADICIONAR AO CARRINHHO</span>
-                </button>
-            </li>
-            <li>
-                <img
-                    src="https://static.netshoes.com.br/produtos/tenis-olympikus-attract-se-815-masculino/26/D22-3836-026/D22-3836-026_zoom1.jpg?ts=1585920125&ims=326x"
-                    alt="tenis"
-                />
-                <strong>Tênis muito legal</strong>
-                <span>R$129,00</span>
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart size={16} color="#FFF"/>
-                    </div>
-                    <span>ADICIONAR AO CARRINHHO</span>
-                </button>
-            </li>
-            <li>
-                <img
-                    src="https://static.netshoes.com.br/produtos/tenis-olympikus-attract-se-815-masculino/26/D22-3836-026/D22-3836-026_zoom1.jpg?ts=1585920125&ims=326x"
-                    alt="tenis"
-                />
-                <strong>Tênis muito legal</strong>
-                <span>R$129,00</span>
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart size={16} color="#FFF"/>
-                    </div>
-                    <span>ADICIONAR AO CARRINHHO</span>
-                </button>
-            </li>
-            <li>
-                <img
-                    src="https://static.netshoes.com.br/produtos/tenis-olympikus-attract-se-815-masculino/26/D22-3836-026/D22-3836-026_zoom1.jpg?ts=1585920125&ims=326x"
-                    alt="tenis"
-                />
-                <strong>Tênis muito legal</strong>
-                <span>R$129,00</span>
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart size={16} color="#FFF"/>
-                    </div>
-                    <span>ADICIONAR AO CARRINHHO</span>
-                </button>
-            </li>
+            {products.map(product => (
+                <li key={product.id}>
+                    <img
+                        src={product.image}
+                        alt={product.title}
+                    />
+                    <strong>{product.title}l</strong>
+                    <span>{product.price}</span>
+                    <button type="button">
+                        <div>
+                            <MdAddShoppingCart size={16} color="#FFF"/>
+                        </div>
+                        <span>ADICIONAR AO CARRINHHO</span>
+                    </button>
+                </li>
+            ))}
         </ProductList>
     )
 }
