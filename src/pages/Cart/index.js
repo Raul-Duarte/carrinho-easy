e501c705e3ea6084c51cf2c393339bd5091d92cd
Raul-Duarte <raulduarte.info@gmail.com>
2021-07-14 21:15:06 -0300

    feat: finalizando o styled da homePage

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,7 +1,30 @@
 import React from "react";
+import {Container, ProductTable, Total} from "./styles";
 
 export default function Cart() {
     return (
-        <h1>Cart</h1>
+        <Container>
+            <ProductTable>
+                <thead>
+                <tr>
+                    <th/>
+                    <th>PRODUTO</th>
+                    <th>QTD</th>
+                    <th>SUBTOTAL</th>
+                    <th/>
+                </tr>
+                </thead>
+                <tbody>
+                
+                </tbody>
+
+            </ProductTable>
+            <footer>
+                <button type="button">Finalizar pedido</button>
+                <Total>
+                    <span>Total</span>
+                </Total>
+            </footer>
+        </Container>
     )
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,95 @@
 import React from "react";
+import {MdAddShoppingCart} from "react-icons/md"
+
+import {ProductList} from "./styles";
 
 export default function Home() {
     return (
-        <h1>Home</h1>
+        <ProductList>
+            <li>
+                <img
+                    src="https://static.netshoes.com.br/produtos/tenis-olympikus-attract-se-815-masculino/26/D22-3836-026/D22-3836-026_zoom1.jpg?ts=1585920125&ims=326x"
+                    alt="tenis"
+                />
+                <strong>Tênis muito legal</strong>
+                <span>R$129,00</span>
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#FFF"/>
+                    </div>
+                    <span>ADICIONAR AO CARRINHHO</span>
+                </button>
+            </li>
+            <li>
+                <img
+                    src="https://static.netshoes.com.br/produtos/tenis-olympikus-attract-se-815-masculino/26/D22-3836-026/D22-3836-026_zoom1.jpg?ts=1585920125&ims=326x"
+                    alt="tenis"
+                />
+                <strong>Tênis muito legal</strong>
+                <span>R$129,00</span>
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#FFF"/>
+                    </div>
+                    <span>ADICIONAR AO CARRINHHO</span>
+                </button>
+            </li>
+            <li>
+                <img
+                    src="https://static.netshoes.com.br/produtos/tenis-olympikus-attract-se-815-masculino/26/D22-3836-026/D22-3836-026_zoom1.jpg?ts=1585920125&ims=326x"
+                    alt="tenis"
+                />
+                <strong>Tênis muito legal</strong>
+                <span>R$129,00</span>
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#FFF"/>
+                    </div>
+                    <span>ADICIONAR AO CARRINHHO</span>
+                </button>
+            </li>
+            <li>
+                <img
+                    src="https://static.netshoes.com.br/produtos/tenis-olympikus-attract-se-815-masculino/26/D22-3836-026/D22-3836-026_zoom1.jpg?ts=1585920125&ims=326x"
+                    alt="tenis"
+                />
+                <strong>Tênis muito legal</strong>
+                <span>R$129,00</span>
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#FFF"/>
+                    </div>
+                    <span>ADICIONAR AO CARRINHHO</span>
+                </button>
+            </li>
+            <li>
+                <img
+                    src="https://static.netshoes.com.br/produtos/tenis-olympikus-attract-se-815-masculino/26/D22-3836-026/D22-3836-026_zoom1.jpg?ts=1585920125&ims=326x"
+                    alt="tenis"
+                />
+                <strong>Tênis muito legal</strong>
+                <span>R$129,00</span>
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#FFF"/>
+                    </div>
+                    <span>ADICIONAR AO CARRINHHO</span>
+                </button>
+            </li>
+            <li>
+                <img
+                    src="https://static.netshoes.com.br/produtos/tenis-olympikus-attract-se-815-masculino/26/D22-3836-026/D22-3836-026_zoom1.jpg?ts=1585920125&ims=326x"
+                    alt="tenis"
+                />
+                <strong>Tênis muito legal</strong>
+                <span>R$129,00</span>
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#FFF"/>
+                    </div>
+                    <span>ADICIONAR AO CARRINHHO</span>
+                </button>
+            </li>
+        </ProductList>
     )
 }
